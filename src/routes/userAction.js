a41a50fda35fb3542771317b998c3fe70842d745
Nico <nlacosta@uc.cl>
2019-04-13 19:39:57 -0400

    added main routes

---
+++ src/routes/userAction.js
@@ -0,0 +1,33 @@
+const KoaRouter = require('koa-router');
+const pkg = require('../../package.json');
+
+const router = new KoaRouter();
+
+router.post('interpretQR','/readQR', async(ctx)=>{
+    
+})
+
+
+router.get('userCalledSeller', '/calledSeller', async(ctx) => {
+    await ctx.render('userAction/calledSeller', {
+        
+    });
+});
+router.get('userCalledList', '/listSKUs', async(ctx) => {
+    //Retrieve list from DB or API
+    const list = ctx.params.listID;
+    const skuresults = undefined;
+    await ctx.render('userAction/listSKUs', {
+        SKUlist: skuresults,
+    });
+});
+router.get('userQR', '/:probID', async(ctx) => {
+    await ctx.render('userAction/qrScanner', {
+        actID: ctx.params.probID,
+    });
+});
+
+
+
+
+module.exports = router;
