8523c02acbb7b406af0c6a2bc64ea29f8cb750d2
Nico <nlacosta@uc.cl>
2019-04-13 18:10:20 -0400

    functional API

---
+++ src/routes/api/index.js
@@ -0,0 +1,9 @@
+const KoaRouter = require('koa-router');
+const jwt = require('koa-jwt');
+const router = new KoaRouter();
+
+router.post("/", async (ctx, next)=>{
+
+});
+
+module.exports = router;
