e597c4f93e678558be375f69583d2aea0d4efade
Nico <nlacosta@uc.cl>
2019-04-13 19:39:43 -0400

    added main routes

--- src/routes/probador.js
+++ src/routes/probador.js
@@ -2,31 +2,59 @@ const KoaRouter = require('koa-router');
 const pkg = require('../../package.json');
 
 const router = new KoaRouter();
-/*
-Ruta para mostrar la pantalla inicial del probador, donde se escanea un producto
-*/
-router.get('probadorGUIstart', '/:probID/start', async(ctx) => {
-    await ctx.render('index', {});
-});
+
 /*
 Ruta para mostrar las prendas del probador en la interfaz superior de seleccion, donde se muestren las recomendaciones
 */
-router.get('probadorGUIaux', '/:probID/chooser', async(ctx) => {
-    await ctx.render('index', {});
+router.get('probadorGUIchooser', '/:probID/chooser', async(ctx) => {
+    await ctx.render('probador/chooser/chooser', {
+        actID: ctx.params.probID,
+    });
 });
 
 /*
+Ruta para mantener una imagen estatica de saludo
 */
-
-router.get('probadorGUIretrieve', '/:probID/retrieve', async(ctx) => {
-    await ctx.render('index', {});
+router.get('probadorGUIchooser', '/:probID/static-chooser', async(ctx) => {
+    await ctx.render('probador/chooser/staticChooser', {
+        actID: ctx.params.probID,
+    });
 });
 
+/*
+Ruta para que el usuario se lleve el listado de prendas que escogio, y pedirlas quiza por ripley.cl
+*/
+
+router.get('probadorGUIretrieve', '/:probID/retrieveQR', async(ctx) => {
+    await ctx.render('probador/chooser/retrieveQR', {
+        actID: ctx.params.probID,
+    });
+});
+/*
+Ruta para mostrar una pantalla de cierre del selector
+*/
+router.get('probadorGUIuse', '/:probID/end', async(ctx) => {
+    await ctx.render('probador/use', {
+        actID: ctx.params.probID,
+    });
+});
 /*
 Ruta para seleccionar el ambiente de prueba, y si el usuario quiere continuar
 */
-router.get('probadorGUIchoose', '/:probID', async(ctx) => {
-    await ctx.render('index', {});
+router.get('probadorGUIuse', '/:probID/use', async(ctx) => {
+    await ctx.render('probador/use', {
+        actSKU: ctx.query.sku,
+        actID: ctx.params.probID,
+    });
+});
+
+/*
+Ruta para mostrar la pantalla inicial del probador, donde se escanea un producto
+*/
+router.get('probadorGUIstart', '/:probID', async(ctx) => {
+    await ctx.render('probador/index', {
+        actID: ctx.params.probID,
+    });
 });
 
 module.exports = router;
