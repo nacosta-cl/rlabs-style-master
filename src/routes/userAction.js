48e7134ed5f23bec55a5cbb56239a171a93093f6
Nico <nlacosta@uc.cl>
2019-04-14 04:35:04 -0400

    fixname

---
+++ src/routes/userAction.js
@@ -0,0 +1,40 @@
+const KoaRouter = require('koa-router');
+const pkg = require('../../package.json');
+
+const router = new KoaRouter();
+
+router.post('interpretQR','/readQR', async(ctx)=>{
+    
+})
+router.get('userAction', '/', async(ctx) => {
+    await ctx.render('index', {
+        
+    });
+});
+
+router.get('userCalledSeller', '/calledSeller', async(ctx) => {
+    const setBuy = ctx.query.set;
+
+    await ctx.render('userAction/calledSeller', {
+        setBuy: setBuy
+    });
+});
+router.get('userCalledList', '/listSKUs', async(ctx) => {
+    //Retrieve list from DB or API
+    const setBuy = ctx.query.set;
+    const skuresults = undefined;
+    await ctx.render('userAction/listSKUs', {
+        setBuy: setBuy,
+        SKUlist: skuresults,
+    });
+});
+router.get('userQR', '/qr', async(ctx) => {
+    await ctx.render('userAction/qrScanner', {
+        actID: ctx.params.probID,
+    });
+});
+
+
+
+
+module.exports = router;
