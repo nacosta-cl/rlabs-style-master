09121c416a63e08dd122d4c738dbf0547ed59272
Nico <nlacosta@uc.cl>
2019-04-13 21:16:48 -0400

    maps fixed

--- src/routes/map.js
+++ src/routes/map.js
@@ -4,7 +4,11 @@ const pkg = require('../../package.json');
 const router = new KoaRouter();
 
 router.get('mainIndex', '/', async(ctx) => {
-    await ctx.render('index', {});
+    await ctx.render('map/index', {});
+});
+
+router.get('mainIndex', '/get', async(ctx) => {
+    await ctx.render('map/map', {});
 });
 
 module.exports = router;
